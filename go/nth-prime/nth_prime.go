9c2d5dd2e5e03fe542357fe5fe2270a247a2c444
sebstrug <sebstrug@users.noreply.github.com>
2021-05-08 19:42:31 +0100

    Update nth prime attempt

--- go/nth-prime/nth_prime.go
+++ go/nth-prime/nth_prime.go
@@ -1,19 +1,50 @@
 package prime
 
-import "fmt"
+import (
+	"math"
+)
+
+// RemoveFromArray removes all multiples of a number n from array a
+func RemoveFromArray(a []int, n int) []int {
+	newArray := []int{}
+	for _, num := range a {
+		if num%n != 0 {
+			newArray = append(newArray, num)
+		}
+	}
+	return newArray
+}
 
 // SieveOfErastothenes finds the prime numbers up to a given limit
 func SieveOfErastothenes(n int) []int {
-	arrayN := make([]int, n-1)
-	for i := range arrayN {
-		arrayN[n] = i+2
+	primes := make([]int, n)
+	primes[0] = 2
+
+	// Make an array of numbers from 2 to n**2
+	arrayN := make([]int, int(math.Pow(float64(n), 2)))
+	for i := 2; i <= int(math.Pow(float64(n), 2)); i++ {
+		arrayN[n] = i
 	}
-	fmt.Printf("Array: %v", arrayN)
+
+	ind := 1
+	for {
+		if ind == n {
+			break
+		}
+		arrayN = RemoveFromArray(arrayN, arrayN[0])
+		primes[ind] = arrayN[0]
+		ind++
+	}
+
 	return arrayN
 }
 
 // Nth gets the nth prime number
 func Nth(n int) (int, bool) {
-	foo := SieveOfErastothenes(20)
-	return foo[0], true
+	if n == 0 {
+		return 0, false
+	}
+
+	primes := SieveOfErastothenes(n)
+	return primes[n], true
 }
