6e30c0488d469db8c546441087ec8436a21bc49a
sebstrug <sebstrug@users.noreply.github.com>
2021-05-25 08:44:41 +0100

    Don't use unnecessary TeamScore struct

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -11,15 +11,10 @@ import (
 
 // Score stores stats for a team
 type Score struct {
+	name   string
 	wins   int
 	draws  int
 	losses int
-}
-
-// TeamScore associates scores with a team
-type TeamScore struct {
-	team  string
-	score Score
 }
 
 // validOutcomes contains the possible outcomes for a game
@@ -47,11 +42,12 @@ func (score *Score) Points() int {
 }
 
 // ParseSingleLine adds the data from a single input line to the overall score mapping
-func ParseSingleLine(scores map[string]*Score, line string) error {
+func ParseSingleLine(scores map[string]Score, line string) error {
 	lineSplit := strings.Split(line, ";")
 	if len(lineSplit) < 3 {
 		return errors.New("invalid number of separators")
 	}
+
 	team1, team2, outcome := lineSplit[0], lineSplit[1], lineSplit[2]
 	if _, ok := scores[team1]; !ok {
 		return errors.New("invalid team name")
@@ -62,14 +58,20 @@ func ParseSingleLine(scores map[string]*Score, line string) error {
 		return errors.New("invalid outcome")
 	}
 
-	scores[team1].Update(outcome)
-	if outcome == "win" {
-		scores[team2].Update("loss")
-	} else if outcome == "loss" {
-		scores[team2].Update("win")
-	} else {
-		scores[team2].Update(outcome)
+	a, b := scores[team1], scores[team2]
+	a.name, b.name = team1, team2
+	switch outcome {
+	case "win":
+		a.wins++
+		b.losses++
+	case "loss":
+		a.losses++
+		b.wins++
+	case "draw":
+		a.draws++
+		b.draws++
 	}
+	scores[team1], scores[team2] = a, b
 	return nil
 }
 
@@ -80,7 +82,7 @@ func Tally(r io.Reader, w io.Writer) error {
 		return err
 	}
 
-	allScores := map[string]*Score{"Allegoric Alaskians": {}, "Blithering Badgers": {}, "Courageous Californians": {}, "Devastating Donkeys": {}}
+	allScores := make(map[string]Score)
 
 	for _, line := range strings.Split(string(data), "\n") {
 		// Ignore comment lines
@@ -95,22 +97,22 @@ func Tally(r io.Reader, w io.Writer) error {
 	}
 
 	// To sort the map by number of points, we must create an array
-	var SortedScores []TeamScore
-	for team, score := range allScores {
-		SortedScores = append(SortedScores, TeamScore{team, *score})
+	var SortedScores []Score
+	for _, score := range allScores {
+		SortedScores = append(SortedScores, score)
 	}
 	// Sort by points, if equal points, sort by name ascending
 	sort.Slice(SortedScores, func(i, j int) bool {
-		if SortedScores[i].score.Points() == SortedScores[j].score.Points() {
-			return SortedScores[i].team < SortedScores[j].team
+		if SortedScores[i].Points() == SortedScores[j].Points() {
+			return SortedScores[i].name < SortedScores[j].name
 		}
-		return SortedScores[i].score.Points() > SortedScores[j].score.Points()
+		return SortedScores[i].Points() > SortedScores[j].Points()
 	})
 
 	fmt.Fprintf(w, `%-31v| %v |  %v |  %v |  %v |  %v`, "Team", "MP", "W", "D", "L", "P")
 	for _, ts := range SortedScores {
 		fmt.Fprintf(w, "\n")
-		fmt.Fprintf(w, `%-31v|  %v |  %v |  %v |  %v |  %v`, ts.team, ts.score.MatchesPlayed(), ts.score.wins, ts.score.draws, ts.score.losses, ts.score.Points())
+		fmt.Fprintf(w, `%-31v|  %v |  %v |  %v |  %v |  %v`, ts.name, ts.MatchesPlayed(), ts.wins, ts.draws, ts.losses, ts.Points())
 	}
 	fmt.Fprintf(w, "\n")
 	return nil
